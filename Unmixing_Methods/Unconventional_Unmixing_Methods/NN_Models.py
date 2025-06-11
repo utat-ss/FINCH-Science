18a46f6e2000e256fc7ecfcf7a5b6527b99f68e3
Ege Artan <103586619+enatrage@users.noreply.github.com>
2025-06-11 18:08:17 +0300

    Update NN_Models.py

--- Unmixing_Methods/Unconventional_Unmixing_Methods/NN_Models.py
+++ Unmixing_Methods/Unconventional_Unmixing_Methods/NN_Models.py
@@ -1,5 +1,5 @@
 """
-This is the file where we define the Naive NN unmixing method,
+This is the file where we define the NN unmixing models,
 we will run the code in the .ipynb file to test it.
 """
 
